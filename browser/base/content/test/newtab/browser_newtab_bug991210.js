e571b5d1b50d698c375e276e7835b24a34b00cbb
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-04-09 13:33:12 -0700

    Refactor unit-test to use AfterLoadProvider

--- browser/base/content/test/newtab/browser_newtab_bug991210.js
+++ browser/base/content/test/newtab/browser_newtab_bug991210.js
@@ -8,12 +8,32 @@ function runTests() {
   let originalPreloadValue = Services.prefs.getBoolPref(PRELOAD_PREF);
   // turn off preload to insure that a newtab page loads
   Services.prefs.setBoolPref(PRELOAD_PREF,false);
-  // note that addNewTabPageTab() calls populateCache upon newtab
-  // "load" event. Since preload is disabled, newtab is loaded anew
-  // and it should cause grid resizing
-  yield addNewTabPageTab();
+  // set up a test provider
+  let afterLoadProvider = {
+    getLinks: function(callback) {
+      this.callback = callback;
+    },
+    addObserver: function() {},
+  };
+  // add afterload provider
+  NewTabUtils.links.addProvider(afterLoadProvider);
+  // open a newtab
+  let tab = gWindow.gBrowser.selectedTab = gWindow.gBrowser.addTab("about:newtab");
+  let browser = tab.linkedBrowser;
+  // call provider callback on "load" and insure grid is resized
+  browser.addEventListener("load", function onLoad() {
+    browser.removeEventListener("load", onLoad, true);
+    // let NewTabUtils.populateCache continue
+    afterLoadProvider.callback([]);
+    // terminate the yield
+    executeSoon(TestRunner.next);
+  }, true);
+  // wait for "load" handler to fire
+  yield true;
   // if Grid is resized it must have _cellMargin defined
   ok(getGrid()._cellMargin != null);
+  // remove afterload provider
+  NewTabUtils.links.removeProvider(afterLoadProvider);
   // restore original value of browser.newtab.preload
   Services.prefs.setBoolPref(PRELOAD_PREF,originalPreloadValue);
 }
